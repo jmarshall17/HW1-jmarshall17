7893d283b440dffef5ba422f212d0ba11eb87c87
Jason Marshall <jmarshall17@uchicago.edu>
2024-01-15 19:13:36 -0600

    Corrected script

--- homework1script.py
+++ homework1script.py
@@ -1,12 +1,29 @@
-##https://docs.spyder-ide.org/5/faq.html#using-packages-installer
-import scipy
+"""The following creates a graph using data from the books
+ Professor Nussbaum has read and traxked using goodreads.com"""
 import plotnine as p9
 import pandas as pd
-data=pd.read_csv(".csv",index_col="Title"); data=data[['My Rating','Average Rating',"Number of Pages","Binding","Date Read"]]     
-a=[];
-data=data.dropna(subset=[ 'Binding' ])
+
+data=pd.read_csv("nussbaum_goodreads_library_export.csv",
+                 index_col="Title")
+
+data=data[['My Rating',
+           'Average Rating',
+           "Number of Pages",
+           "Binding",
+           "Date Read"]]
+
+a=[]
+data=data.dropna(subset=['Binding'])
+
 for book in data.index:
-    if (data.loc[book]["Binding"] in [ "Hardcover" ,"Kindle Edition" ,"Mass Market Paperback" ,"Paperback" ,"ebook" ])==True:
-        a.append(book) ##364
-data=data.loc [ a ]
-p9.ggplot(data,p9.aes(x="Number of Pages",fill="My Rating"))+p9.geom_histogram()+p9.facet_grid('My Rating~.')
+    if (data.loc[book]["Binding"] in ["Hardcover",
+                                      "Kindle Edition",
+                                      "Mass Market Paperback",
+                                      "Paperback","ebook"]) is True:
+        a.append(book)
+
+data=data.loc[a]
+
+plot = p9.ggplot(data,
+                 p9.aes(x="Number of Pages",
+                        fill="My Rating"))+p9.geom_histogram()+p9.facet_grid('My Rating~.')
